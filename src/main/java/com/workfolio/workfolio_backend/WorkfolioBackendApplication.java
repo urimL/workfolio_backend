ae1442ddfa06192ba9a48d6909b362b0e464fd67
toulim <toulim0525@ewhain.net>
2024-01-23 11:21:45 +0900

    Initial commit

---
+++ src/main/java/com/workfolio/workfolio_backend/WorkfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workfolio.workfolio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkfolioBackendApplication.class, args);
+	}
+
+}
